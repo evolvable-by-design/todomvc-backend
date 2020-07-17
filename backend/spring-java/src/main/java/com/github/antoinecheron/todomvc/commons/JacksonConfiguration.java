082c1e8f23f16633e3e724d741ae9355a09c342b
Antoine Cheron <cheron.antoine@gmail.com>
2020-07-17 10:43:38 +0200

    Add Java 8 modules to Jackson

--- backend/spring-java/src/main/java/com/github/antoinecheron/todomvc/commons/JacksonConfiguration.java
+++ backend/spring-java/src/main/java/com/github/antoinecheron/todomvc/commons/JacksonConfiguration.java
@@ -1,7 +1,11 @@
 package com.github.antoinecheron.todomvc.commons;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
 import com.fasterxml.jackson.databind.module.SimpleModule;
+import com.fasterxml.jackson.datatype.jdk8.Jdk8Module;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.module.paramnames.ParameterNamesModule;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -22,6 +26,11 @@ public class JacksonConfiguration {
     applicationSpecificSerializers.addSerializer(new HypermediaRepresentation.Serializer());
 
     objectMapper.registerModule(applicationSpecificSerializers);
+    objectMapper.registerModule(new ParameterNamesModule());
+    objectMapper.registerModule(new Jdk8Module());
+    objectMapper.registerModule(new JavaTimeModule());
+
+    objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS, false);
 
     return objectMapper;
   }
